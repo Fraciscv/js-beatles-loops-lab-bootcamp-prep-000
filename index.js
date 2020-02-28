b1352398cac081067ae2b51d1e9aeb60a726edc8
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 16:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ function theBeatlesPlay(musicians, instruments) {
   var array = []
 
   for (var i = 0, x = musicians.length; i < x; i++) { //for loop which iterates over the array of musicians.
-    array.push(`${musicians[i]} plays ${instruments[i]}`); //returns an array of strings containing what instruments each musician plays.
+    array.push(`${musicians[i]} plays ${instruments[i]}`); //create a string using the _first_
+  //index of the musicians array **and** the first index of the instruments
   }
 
-  return array
+  return array//The function should return the array of new strings containing what
+  //instruments each musician plays.
 }
 
 function johnLennonFacts(facts) {
-  var shoutedFacts = []
+  var Facts = []
 
   let i = 0
   while (i < facts.length) {
-    shoutedFacts.push(`${facts[i]}!!!`); //returns an array of strings with exclamation points
+    Facts.push(`${facts[i]}!!!`); //returns an array of strings with exclamation points
     i++
   }
 
