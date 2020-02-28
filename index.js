64979848e00ff542f9838b334089bd4388a8c0c9
Francisco Andaur <francisco.andaur@icloud.com>
2020-02-28 15:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-// add solution here
-function theBeattlePlay(musicians, instruments) {
-      var array = []
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
 
-      for(let i = 0; l = musicians.length; i < l; i++){
-        array.push(`${musicians[i]} plays ${instruments[i]}`)
-      }
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 
-      retirm array;
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return array
 }
